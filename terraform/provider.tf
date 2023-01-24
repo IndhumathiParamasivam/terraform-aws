f3452571115b44c08b453cb734c8b8a1544d7a3c
indhu <indhu@LAPTOP-761FVFV7>
2023-01-24 17:24:12 +0530

    creating files for aws terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "S3"
+  {
+  region = "ap-south-1"
+  bucket = "statefiles-terraform-github"
+  key    = "test.tfstate"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
